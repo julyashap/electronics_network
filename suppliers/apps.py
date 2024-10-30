c9c6e256458ab5be8bf70c493d7a7076b4dcc7ea
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-30 22:28:13 +0400

    Replace deliveries app to suppliers app

--- suppliers/apps.py
+++ suppliers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DeliveriesConfig(AppConfig):
+class SuppliersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'deliveries'
+    name = 'suppliers'
