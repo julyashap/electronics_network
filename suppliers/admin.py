848709a8d5d920a24af2c7d0c8eed75df7fe6219
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-31 16:30:42 +0400

    Add Supplier and Product models to admin

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from suppliers.models import Supplier, Product
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'supplier_link', 'debt_to_supplier', 'level', 'email', 'city',)
+    list_filter = ('city',)
+    search_fields = ('city',)
+
+    actions = ['reset_dept_to_supplier']
+
+    def reset_dept_to_supplier(self, request, queryset):
+        queryset.update(debt_to_supplier=0)
+        self.message_user(request, 'Задолженность для выбранных объектов успешно сброшена!')
+
+    reset_dept_to_supplier.short_description = 'Сбросить размер задолженности'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'model', 'realized_at', 'supplier',)
+    list_filter = ('name',)
