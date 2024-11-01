198943cdf74cd615541fb1a65bb159b2a0765937
Julya Shapaeva <shapaeva03@mail.ru>
2024-11-01 16:36:41 +0400

    Add CodePhrase model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, CodePhrase
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'email',)
     list_filter = ('email',)
+
+
+@admin.register(CodePhrase)
+class CodePhraseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'company_name', 'codephrase',)
+    search_fields = ('company_name',)
