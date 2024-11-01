d800d9484892b3ed89d106a69d8dafcc0de1d66d
Julya Shapaeva <shapaeva03@mail.ru>
2024-11-01 16:35:19 +0400

    Add urlpatterns for Product model views

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from suppliers.apps import SuppliersConfig
 from suppliers.views import SupplierListAPIView, SupplierDestroyAPIView, SupplierRetrieveAPIView, \
-    SupplierCreateAPIView, SupplierUpdateAPIView
+    SupplierCreateAPIView, SupplierUpdateAPIView, ProductCreateAPIView, ProductUpdateAPIView, ProductDestroyAPIView
 
 app_name = SuppliersConfig.name
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('create/', SupplierCreateAPIView.as_view(), name='supplier_create'),
     path('update/<int:pk>/', SupplierUpdateAPIView.as_view(), name='supplier_update'),
     path('destroy/<int:pk>/', SupplierDestroyAPIView.as_view(), name='supplier_destroy'),
+
+    path('product/create/', ProductCreateAPIView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateAPIView.as_view(), name='product_update'),
+    path('product/destroy/<int:pk>/', ProductDestroyAPIView.as_view(), name='product_destroy'),
 ]
