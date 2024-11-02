1043bde9c049b13520c9b4cde9a451e6cba686f0
Julya Shapaeva <shapaeva03@mail.ru>
2024-11-02 14:02:07 +0400

    Replace CodePhraseQuerySerializer to CodePhraseSerializer and write docstrings

--- users/views.py
+++ users/views.py
@@ -3,16 +3,18 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from users.models import User
 from users.permissions import IsCorrectCodePhrase
-from users.serializers import UserSerializer, CodePhraseQuerySerializer
+from users.serializers import UserSerializer, CodePhraseSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
+    """Представление для создания объекта модели User"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny & IsCorrectCodePhrase]
 
     @swagger_auto_schema(
-        query_serializer=CodePhraseQuerySerializer()
+        query_serializer=CodePhraseSerializer()
     )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
@@ -24,24 +26,28 @@ class UserCreateAPIView(generics.CreateAPIView):
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
+    """Представление для удаления объекта модели User"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny & IsCorrectCodePhrase]
 
     @swagger_auto_schema(
-        query_serializer=CodePhraseQuerySerializer()
+        query_serializer=CodePhraseSerializer()
     )
     def delete(self, request, *args, **kwargs):
         return super().delete(request, *args, **kwargs)
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
+    """Представление для обновления объекта модели User"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny & IsCorrectCodePhrase]
 
     @swagger_auto_schema(
-        query_serializer=CodePhraseQuerySerializer()
+        query_serializer=CodePhraseSerializer()
     )
     def put(self, request, *args, **kwargs):
         return super().put(request, *args, **kwargs)
@@ -56,12 +62,14 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """Представление для просмотра объекта модели User"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny & IsCorrectCodePhrase]
 
     @swagger_auto_schema(
-        query_serializer=CodePhraseQuerySerializer()
+        query_serializer=CodePhraseSerializer()
     )
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
