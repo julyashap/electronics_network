81d3248af3aead3d2e462fa70e2d293b0afd14a9
Julya Shapaeva <shapaeva03@mail.ru>
2024-11-02 14:01:03 +0400

    Update IsCorrectCodePhrase permission with CodePhraseSerializer

--- users/permissions.py
+++ users/permissions.py
@@ -1,12 +1,23 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 from users.models import CodePhrase
+from users.serializers import CodePhraseSerializer
 
 
 class IsCorrectCodePhrase(permissions.BasePermission):
+    """Класс проверки прав доступа пользователя путем проверки введенной им кодовой фразы"""
+
     def has_permission(self, request, view):
-        company_name = request.data.get('company_name')
-        codephrase = request.data.get('codephrase')
+        data = {
+            'company_name': request.query_params.get('company_name'),
+            'codephrase': request.query_params.get('codephrase')
+        }
+
+        serializer = CodePhraseSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        company_name = serializer.validated_data['company_name']
+        codephrase = serializer.validated_data['codephrase']
 
         if CodePhrase.objects.filter(company_name=company_name, codephrase=codephrase):
             return True
