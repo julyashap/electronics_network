d88c6723a49415c1be390b47c47cc6b067f79a7a
Julya Shapaeva <shapaeva03@mail.ru>
2024-11-02 14:02:57 +0400

    Add self.query_params to setUp in UserAPITestCase and write docstring

--- users/tests.py
+++ users/tests.py
@@ -5,53 +5,81 @@ from users.models import User, CodePhrase
 
 
 class UserAPITestCase(APITestCase):
+    """Класс тестирования представлений модели User"""
+
     def setUp(self):
         self.codephrase = CodePhrase.objects.create(company_name='test_company', codephrase='testcph')
         self.user = User.objects.create(email='test_user@example.com', password='test_password')
 
-    def test_create_user_with_correct_codephrase(self):
-        create_data = {
-            'email': 'new_user@example.com',
-            'password': 'new_password',
+        self.query_params = {
             'company_name': 'test_company',
             'codephrase': 'testcph'
         }
-        response = self.client.post(reverse('users:user_create'), data=create_data)
+
+    def test_create_user_with_correct_codephrase(self):
+        create_data = {
+            'email': 'new_user@example.com',
+            'password': 'new_password'
+        }
+
+        response = self.client.post(
+            reverse('users:user_create'),
+            query_params=self.query_params,
+            data=create_data,
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 2)
 
     def test_create_user_with_incorrect_codephrase(self):
         create_data = {
             'email': 'new_user@example.com',
-            'password': 'new_password',
-            'company_name': 'test_company',
-            'codephrase': 'testcph_incorrect'
+            'password': 'new_password'
         }
-        response = self.client.post(reverse('users:user_create'), data=create_data, format='json')
+
+        incorrect_query_params = {
+            'company_name': 'test_company',
+            'codephrase': 'testcphinc'
+        }
+
+        response = self.client.post(
+            reverse('users:user_create'),
+            query_params=incorrect_query_params,
+            data=create_data,
+            format='json'
+        )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(response.data, {
-            "detail": "Кодовая фраза 'testcph_incorrect' для test_company неверна!"
+            "detail": "Кодовая фраза 'testcphinc' для test_company неверна!"
         })
 
     def test_update_user(self):
         update_data = {
             'email': 'updated_user@example.com',
-            'password': 'updated_password',
-            'company_name': 'test_company',
-            'codephrase': 'testcph'
+            'password': 'updated_password'
         }
-        response = self.client.patch(reverse('users:user_update', args=[self.user.pk]), data=update_data)
+
+        response = self.client.patch(
+            reverse('users:user_update', args=[self.user.pk]),
+            query_params=self.query_params,
+            data=update_data
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.user.refresh_from_db()
         self.assertEqual(self.user.email, 'updated_user@example.com')
 
-    def test_destroy_user(self):
-        destroy_data = {
-            'company_name': 'test_company',
-            'codephrase': 'testcph'
-        }
+    def test_retrieve_user(self):
+        response = self.client.get(
+            reverse('users:user_retrieve', args=[self.user.pk]),
+            query_params=self.query_params
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], self.user.email)
 
-        response = self.client.delete(reverse('users:user_destroy', args=[self.user.pk]), data=destroy_data)
+    def test_destroy_user(self):
+        response = self.client.delete(
+            reverse('users:user_destroy', args=[self.user.pk]),
+            query_params=self.query_params
+        )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(User.objects.count(), 0)
